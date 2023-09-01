0370eb6353c4affa2d751a5c6288132009d636f0
dineshkalekar <141424600+dineshkalekar@users.noreply.github.com>
2023-09-01 21:54:43 +0530

    Add files via upload
    back up on 01sep 2023

---
+++ Priority.java
@@ -0,0 +1,18 @@
+package KeywordsInTestNG;
+
+import org.testng.annotations.Test;
+
+public class Priority {
+	
+	@Test
+	public void TestA() {
+		System.out.println("Test A ");
+		
+	}
+	@Test()
+public void TestB() {
+	System.out.println("Test B");
+		
+	}
+
+}
